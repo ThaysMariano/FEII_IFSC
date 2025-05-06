a08e8594f690f616aef3d5dca1428082e43a0cf1
ThaysMariano <thaysdasilvamariano@gmail.com>
2025-05-05 21:36:44 -0300

    Inicio atividades slides

---
+++ tyFuncoes/script.ts
@@ -0,0 +1,16 @@
+
+//Slides ty parte 3 exercicios
+
+//01
+type primeiroEL = (array: Array<T>) => <T>;
+
+const primeiroElemento: primeiroEL = (array)=> array[0];
+
+
+//02
+type exibir ={
+    (a: number): number;
+    (b: String): String;
+}
+
+
