32c9a34614279072b591e8e338ad55c6671785e0
ThaysMariano <thaysdasilvamariano@gmail.com>
2025-06-25 22:26:02 -0300

    Feito teste de funcao

--- receitas/src/componentes/estrela/Estrela.test.tsx
+++ receitas/src/componentes/estrela/Estrela.test.tsx
@@ -1,36 +1,61 @@
-import { test, expect } from "vitest";
-import { render, screen } from "@testing-library/react"
+import { test, expect, vi, describe } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react"
 import "@testing-library/jest-dom/vitest"
 import Estrela from ".";
 
-test("Deve renderizar com a cor lightgray quando não selecianada", ()=>{
+describe("Deve renderizar", () => {
 
-    //arrange
-    //renderiza a estrela
-    render(<Estrela></Estrela>);
+    test("com a cor lightgray quando não selecianada", () => {
 
-    //act
-    //pega o elemento da tela para analise
-    const estrela = screen.getByTestId("estrela");    
+        //arrange
+        //renderiza a estrela
+        render(<Estrela></Estrela>);
 
-    //assert
-    //verificacao com o que se espera
-    expect(estrela).toHaveAttribute("color", "lightgray");
-})
+        //act
+        //pega o elemento da tela para analise
+        const estrela = screen.getByTestId("estrela");
+
+        //assert
+        //verificacao com o que se espera
+        expect(estrela).toHaveAttribute("color", "lightgray");
+    });
 
 
-test("Deve renderizar com a cor coral quando selecianada", ()=>{
+    test("com a cor coral quando selecianada", () => {
 
-    //arrange
-    //renderiza a estrela
-    render(<Estrela selecionada={true}></Estrela>);
+        //arrange
+        //renderiza a estrela
+        render(<Estrela selecionada={true}></Estrela>);
 
-    //act
-    //pega o elemento da tela para analise
-    const estrela = screen.getByTestId("estrela");    
+        //act
+        //pega o elemento da tela para analise
+        const estrela = screen.getByTestId("estrela");
 
-    //assert
-    //verificacao com o que se espera
-    expect(estrela).toHaveAttribute("color", "coral");
-})
+        //assert
+        //verificacao com o que se espera
+        expect(estrela).toHaveAttribute("color", "coral");
+    });
 
+
+
+    test("e chamar 'aoSelecionar' quando pressionada", () => {
+
+        //arrange
+        //renderiza a estrela
+
+        const mockSelecionar = vi.fn();
+        render(<Estrela aoSelecionar={mockSelecionar}></Estrela>);
+
+        //act
+        //pega o elemento da tela para analise
+        const estrela = screen.getByTestId("estrela");
+        fireEvent.click(estrela); //simular evento de clique
+
+
+        //assert
+        //verificacao com o que se espera
+        expect(mockSelecionar).toHaveBeenCalledTimes(1);
+
+    });
+
+});
